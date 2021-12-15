ddf4e7861975a60b818d8e7a5a24c1925e27f91f
Finn Westergren <fwesterg5@gmail.com>
2021-12-15 14:03:06 -0500

    setup iis

---
+++ Server/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Produces("application/json")]
+        [Route("[controller]/OneThroughFive")]
+        public ActionResult<IEnumerable<int>> OneThroughFive()
+        {
+            return new JsonResult(Enumerable.Range(1, 5));
+        }
+    }
+}
